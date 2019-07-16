928aeaad2db3d08b4ae661721075b46444ec6535
eunicejihyun <52475753+eunicejihyun@users.noreply.github.com>
2019-07-16 15:32:02 -0400

    Update Number Guessing Game.py
    Final Version from 7/16

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,23 +1,38 @@
 # Number Guessing Game as seen on Al's YouTube Channel
 
 
-print ('What is your name, friend?')
-name = input()
+
+
+print ('How challenging do you want this game to be?')
+while True:
+    try:
+        maximum = int(input('Level 1 - 1000 : '))
+        if maximum not in range (1,1001):
+            raise ValueError
+        break
+    except ValueError:
+        print ('Choose a number from 1 to 1000')
+        continue
+
     
 def game():      
     import random
 
-    number = random.randint(1,20)
+    number = random.randint(1,int(maximum))
 
-    print ('I am thinking of a number between 1 and 20, ' + name + '. What is it?')
+    print ('I am thinking of a number between 1 and ' + str(maximum) + '. What is it?')
 
-    for attempts in range(1,6): #5 tries
+    for attempts in range(1,11): #10 tries
         global guess
-        try:
-            guess = int(input())
-        except ValueError:
-            print ('Use numbers. Try again.')
-            guess = int(input())
+        while True:
+            try:
+                guess = int(input('Guess: '))
+                if guess < 1 or guess > maximum:
+                    raise ValueError
+                break
+            except ValueError:
+                print ('Use numbers from 1 to ' + str(maximum) + '. Try again.')
+                continue          
         if guess > number:
             print ('Too high.')
         elif guess < number:
@@ -26,19 +41,28 @@ def game():
             print ('Great job! You got it right in ' + str(attempts) + ' tries!')
             break
      
-    if attempts == 5 and guess != number:
-        print ('The number was ' + str(number) + '. Want to play another round? y/n')
+    if attempts == 10 and guess != number:
+        print ('Nice try, but the number was ' + str(number) + '. Want to play another round? y/n')
     else:
-        print  ('Want to play another round? y/n')
-
-game()
+       print  ('Great job! Want to play another round? y/n')
 
 
-response = input()
+def respond():
+    while True:
+        global response
+        response = input()
+        try:
+            if response not in ['y', 'n']:
+                raise ValueError
+            break
+        except ValueError:
+            print ('Please use either y or n.')
+
+response = 'y'
 
 while response == 'y':
     game()
-    response = input()
+    respond()
 
 
 print ('Thanks for playing')
