0f6971a3510ca839091cdcdd8e12a6609de66f10
EJ <eunicejihyun@gmail.com>
2022-01-12 21:34:18 -0500

    Uploading Day 1 Project

---
+++ 100DaysOfCode/001_bandNameGenerator.py
@@ -0,0 +1,29 @@
+import random
+
+print("Welcome to the Band Name Generator!\n")
+
+city = input("Which city are you from?\n    ")
+pet = input("What's your pet's name?\n    ")
+color = input("What's your favorite color?\n    ")
+number = str(input("What's your favorite number?\n    "))
+food = input("What's your favorite food?\n    ")
+season = input("What's your favorite season?\n    ")
+animal = input("What's your favorite animal?\n    ")
+
+info = [city, pet, color, number, food, season, animal]
+
+bandNames = []
+
+# Print multiple options for band names
+for i in range(5):
+    x = random.randrange(len(info))
+    y = random.randrange(len(info))
+    # Ensure that x != y
+    while y == x:
+        y = random.randrange(6)
+        print(y)
+    bandNames.append(info[x]+" "+info[y])
+
+print("\nPotential Band Names:")
+for i in bandNames:
+    print(" " + i)
