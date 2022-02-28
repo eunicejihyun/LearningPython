7c2d1cc31f178f89298eee01b09acc08d4d22c13
EJ <eunicejihyun@gmail.com>
2022-02-28 18:49:08 -0500

    day 48

--- 100DaysOfCode/048_bootstrapBlog/048_bootstrapBlog.py
+++ 100DaysOfCode/048_bootstrapBlog/048_bootstrapBlog.py
@@ -1,5 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib as s
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# EMAIL PORTION _________________________________________________________________________
+FROM_GMAIL = "your email"
+PASSWORD = "your password"
+EMAIL_LIST = ["your list of emails"]
+
+
+def send_email(text):
+    message = MIMEMultipart("alternative")
+    message['Subject'] = "New message from your blog!"
+    message['From'] = FROM_GMAIL
+    message['To'] = ", ".join(EMAIL_LIST)
+
+    part1 = MIMEText(text, "plain")
+
+    message.attach(part1)
+
+    with s.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=FROM_GMAIL, password=PASSWORD)
+        connection.send_message(msg=message,
+                                from_addr=FROM_GMAIL,
+                                to_addrs=EMAIL_LIST)
+
+
+# _________________________________________________________________________
+
 
 app = Flask(__name__)
 
@@ -16,11 +46,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
-
-
 @app.route('/post/<int:id>')
 def blog(id):
     requested_post = None
@@ -28,6 +53,22 @@ def blog(id):
         if post['id'] == id:
             requested_post = post
     return render_template("post.html", post=requested_post)
+
+
+@app.route('/contact', methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        name = data['name']
+        email = data['email']
+        phone = data['phone']
+        message = data['message']
+
+        email_content = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+        send_email(email_content)
+        return "Successfully sent your message"
+    else:
+        return render_template("contact.html")
 
 
 if __name__ == "__main__":
