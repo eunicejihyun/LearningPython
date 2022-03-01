3a8ca16ddf34b9d6dbb8c340d912b81762f5936e
EJ <eunicejihyun@gmail.com>
2022-02-28 20:28:43 -0500

    day 48

---
+++ 100DaysOfCode/048_flaskWTFintro/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+
+app.secret_key = "some secret string"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        email = login_form.email.data
+        pw = login_form.password.data
+        if email=="admin@email.com" and pw=="12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    Bootstrap(app)
+    app.run(debug=True)
